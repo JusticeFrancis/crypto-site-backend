6a59a3ff96cb968b38e061fbdeebf2b367be8e24
justicefrancis <cjfrancisf@gmail.com>
2022-04-20 18:50:38 +0100

    n

--- jobs/updateWallet.js
+++ jobs/updateWallet.js
@@ -18,24 +18,24 @@ async function main() {
         mongoose.connect(url,connectionParams)
         .then( () => {
             console.log('Connected to the database ')
+            if(workerData.coin === '0'){
+                const wallet = await Wallet.findById(workerData.id)
+                console.log(wallet)
+                wallet.balanceUSDT = Number(wallet.balanceUSDT) * 2
+                wallet.save()
+            }
+            if(workerData.coin === '1'){
+                const wallet = await Wallet.findById(workerData.id)
+                console.log(wallet)
+                wallet.balanceBTC = Number(wallet.balanceBTC) * 2
+                wallet.save()
+            }
             
         })
         .catch( (err) => {
             console.error(`Error connecting to the database. n${err}`);
         })
-    console.log(workerData)
-    if(workerData.coin === '0'){
-        const wallet = await Wallet.findById(workerData.id)
-        console.log(wallet)
-        wallet.balanceUSDT = Number(wallet.balanceUSDT) * 2
-        wallet.save()
-    }
-    if(workerData.coin === '1'){
-        const wallet = await Wallet.findById(workerData.id)
-        console.log(wallet)
-        wallet.balanceBTC = Number(wallet.balanceBTC) * 2
-        wallet.save()
-    }
+    
 
   
 }
