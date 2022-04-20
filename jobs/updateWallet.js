03fed3fa70841c8ab028310a37c31f6733f8ca21
justicefrancis <cjfrancisf@gmail.com>
2022-04-20 18:11:11 +0100

    l

--- jobs/updateWallet.js
+++ jobs/updateWallet.js
@@ -4,7 +4,7 @@ const Wallet = require("../models/walletModel");
 async function main() {
     console.log(workerData)
     if(workerData.coin === '0'){
-        const wallet = await Wallet.findById(ObjectId(workerData.id))
+        const wallet = await Wallet.findById(workerData.id)
         wallet.balanceUSDT = Number(wallet.balanceUSDT) * 2
         wallet.save()
     }
