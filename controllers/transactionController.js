1cb6ea8d07e62a5ca130b39afdc9ff4617021d48
justicefrancis <cjfrancisf@gmail.com>
2022-04-23 11:17:26 +0100

    m

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -144,6 +144,7 @@ async function approveTransaction(req, res, next) {
                 const bree2 = new Bree({
                     jobs : [{
                     name : 'dailyWallet',
+                    timeout : '30s',
                     interval : '1m',
                     worker : {
                         workerData : {
